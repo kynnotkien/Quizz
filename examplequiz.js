63a3c4c28410b375e5d9cc482f7e7fadc7dfb2d8
Audrey912-7d25 <audreyissilly@gmail.com>
2024-11-23 16:57:34 +0000

    example quiz!

---
+++ examplequiz.js
@@ -0,0 +1,28 @@
+//set the name of the quiz
+var QuizName = "example name"
+
+//define number of questions
+var QuesAmount = 3
+
+//array of questions
+var Questions = [
+    `example question 1`,
+    `example question 2`,
+    `example question 3`,
+];
+
+//array of answers, no answer is represented with a "-", but must be 4 answers per question
+var Answers = [
+    `example question 1`,
+    `example question 2`,
+    `example question 3`,
+    `example question 4`,
+    `example question 5`,
+    `example question 6`,
+    `example question 7`,
+    `example question 8`,
+    `example question 9`,
+    `example question 10`,
+    `example question 11`,
+    `example question 12`,
+]
