1ca0fc0f805e16f2de7f1ecf2f85567b653bc900
Audrey912-7d25 <audreyissilly@gmail.com>
2024-11-23 18:33:55 +0000

    fixed boolean in example quizes

--- quiz.js
+++ quiz.js
@@ -11,25 +11,43 @@ class Question {
 
 // Answer class
 class Answer {
-    constructor(text, isCorrect) {
+    constructor(text) {
         this.text = text; // text for the answer
+    }
+}
+
+class AnswerBool {
+    constructor(isCorrect) {
         this.isCorrect = isCorrect; //boolean if the question is right or wrong
     }
 }
 
 // make answers
-const answer1 = new Answer("335", false);
-const answer2 = new Answer("456", false);
-const answer3 = new Answer("446", true);
-const answer4 = new Answer("426", false);
-const answer5 = new Answer("788", true);
-const answer6 = new Answer("778", false);
-const answer7 = new Answer("768", false);
-const answer8 = new Answer("758", false);
-const answer9 = new Answer("80", false);
-const answer10 = new Answer("400", true);
-const answer11 = new Answer("800", false);
-const answer12 = new Answer("4000", false);
+const answer1 = new Answer("335",);
+const answer2 = new Answer("456",);
+const answer3 = new Answer("446",);
+const answer4 = new Answer("426",);
+const answer5 = new Answer("788",);
+const answer6 = new Answer("778",);
+const answer7 = new Answer("768",);
+const answer8 = new Answer("758",);
+const answer9 = new Answer("80",);
+const answer10 = new Answer("400",);
+const answer11 = new Answer("800",);
+const answer12 = new Answer("4000",);
+
+const answerbool1 = new AnswerBool(false);
+const answerbool2 = new AnswerBool(false);
+const answerbool3 = new AnswerBool(true);
+const answerbool4 = new AnswerBool(false);
+const answerbool5 = new AnswerBool(true);
+const answerbool6 = new AnswerBool(false);
+const answerbool7 = new AnswerBool(false);
+const answerbool8 = new AnswerBool(false);
+const answerbool9 = new AnswerBool(false);
+const answerbool10 = new AnswerBool(true);
+const answerbool11 = new AnswerBool(false);
+const answerbool12 = new AnswerBool(false);
 
 // Create a question with answers
 const question1 = new Question("What is the sum of 130+125+191?", [answer1, answer2, answer3, answer4]);
